997130de111f024fdb574d399d60af9ac7eb1b53
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-10-30 20:32:58 +0900

    10/30
    トップページ画像変化

--- app/assets/javascripts/topPage2.js
+++ app/assets/javascripts/topPage2.js
@@ -23,6 +23,33 @@ $(function(){
     $(".fa-arrow-alt-circle-right").show();              //右矢印アイコン表示
   })
 
+  //トップ画像入れ替え（2枚以上時表示）
+  // var rimokon;
+  // var topimage_flg = 0;
+  // var hover_flg = 0;
+  // $("#topimage2").hide();
+  // function loop(){
+  //   if(topimage_flg == 0 && hover_flg == 0){
+  //     $("#topimage1").fadeOut(2500);
+  //     $("#topimage2").fadeIn(2500);
+  //     console.log("上");
+  //     topimage_flg = 1
+  //   }else if(topimage_flg == 1 && hover_flg == 0){
+  //     $("#topimage2").fadeOut(2500);
+  //     $("#topimage1").fadeIn(2500);
+  //     console.log("下");
+  //     topimage_flg = 0
+  //   }
+  //   $('.header_top_right').hover(function() {
+  //     hover_flg = 1
+  //   }, function() {
+  //     hover_flg = 0
+  //   });
+  //   rimokon = setTimeout(loop,5000)
+  // }
+  // loop();
+
+  //Photo自動スクロール
   $(function(){
 		$('.photo_main_scroll1').infiniteslide({
 			'speed': 30, //speed this is px/min
