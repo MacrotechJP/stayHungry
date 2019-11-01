db1dd48662a483b622e7aee3bced1e99f7cae236
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-11-02 02:15:38 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Invoicer::Application.configure do 
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: "tanakakoyo1999@gmail.com",
+    password: "aovosplpekpfuizy",
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    authentication: "login",
+    enable_starttls_auto: true
+  }
+end
