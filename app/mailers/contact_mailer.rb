c0f12d3b7aeb0c12dc2aceaa9c7ac1ee9e61ba76
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-11-02 16:45:32 +0900

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def send_when_admin_reply(contact) #メソッドに対して引数を設定
     @contact = contact #お問い合わせ情報
-    mail to: "tanakakoyo1999@gmail.com,leh.shimodatks@gmail.com", subject: '【StayHungryHP】お問い合わせの連絡'
+    mail to: "tanakakoyo1999@gmail.com,leh.shimodatks1@gmail.com", subject: '【StayHungryHP】お問い合わせの連絡'
   end
 
 end
